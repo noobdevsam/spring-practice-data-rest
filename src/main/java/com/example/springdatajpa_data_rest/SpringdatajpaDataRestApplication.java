7ef2acefde400b596ccad8cb28e74362288903a5
noobdevsam <mdshoykotislam05@gmail.com>
2024-10-27 21:28:37 +0600

    initial commit

---
+++ src/main/java/com/example/springdatajpa_data_rest/SpringdatajpaDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaDataRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdatajpa_data_rest/SpringdatajpaDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
